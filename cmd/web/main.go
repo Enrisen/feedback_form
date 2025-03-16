dde4087300065a273bdb04a2f081702b854e25fe
Enrisen <2019120183@ub.edu.bz>
2025-03-16 09:26:47 -0600

    Connection pool to database feedback successfully established.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"html/template"
 	"log/slog"
 	"os"
+	"time"
+
+	//pq package
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -15,9 +21,22 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://feedback:tapirhorse@localhost/feedback?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		logger.Error(err.Error())
@@ -35,4 +54,29 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(),
+		5*time.Second)
+	defer cancel()
+
+	// let's test if the connection pool was created
+	// we trying pinging it with a 5-second timeout
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
+
 }
